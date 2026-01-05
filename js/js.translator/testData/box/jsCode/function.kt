d879683fbe30c7ba1970897100d18941b1ba762a
Andrii Rublov <andrii.rublov@jetbrains.com>
2026-01-05 21:56:16 +0100

    [K/JS] KT-83456: Support ES6 rest function parameters in `js()`` calls
    ^KT-83456 Fixed

--- js/js.translator/testData/box/jsCode/function.kt
+++ js/js.translator/testData/box/jsCode/function.kt
@@ -1,23 +1,31 @@
-package foo
+// WITH_STDLIB
+
 // FILE: a.kt
 
-fun callWithArgs(sumFunc: (Int, Int) -> Int, a: Int, b: Int): Int {
-    return sumFunc(a, b)
 object Kt {
     fun sum(a: Int, b: Int): Int = a + b
+    fun sumRest(vararg args: Int): Int = args.sum()
+    fun greetingRest(greeting: String, vararg args: String): String = "$greeting, ${args.joinToString(", ")}"
 }
+
 object Js {
     val sum = js("function (a, b) { return a + b; }")
+    val sumRest = js("function (...args) { return args.reduce((a, b) => a + b); }")
+    val namedSumRest = js("function sum(...args) { return args.reduce((a, b) => a + b); }")
+    val greetingRest = js("function (greeting, ...args) { return greeting + ', ' + args.join(', '); }")
 }
 
 // FILE: b.kt
 // RECOMPILE
 
 fun box(): String {
-    val kotlinSum: (Int, Int) -> Int = { a, b -> a + b}
-    val jsSum: (Int, Int) -> Int = js("function (a, b) { return a + b; }")
-    assertEquals(callWithArgs(kotlinSum, 1, 2), callWithArgs(jsSum, 1, 2))
     assertEquals(Kt.sum(1, 2), Js.sum(1, 2))
+    assertEquals(Kt.sumRest(1, 2, 3), Js.sumRest(1, 2, 3))
+    assertEquals(Kt.sumRest(1, 2, 3), Js.namedSumRest(1, 2, 3))
+    assertEquals(
+        Kt.greetingRest("Hello", "K/JS", "Kotlin Multiplatform", "Compose Multiplatform"),
+        Js.greetingRest("Hello", "K/JS", "Kotlin Multiplatform", "Compose Multiplatform")
+    )
 
     return "OK"
 }
