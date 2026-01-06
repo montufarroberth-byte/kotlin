27a19efe4c2ba87b1fc83556223702198eb52601
Pavel Kunyavskiy <pavel.kunyavskiy@jetbrains.com>
2026-01-06 14:42:22 +0100

    fixup! [JVM] Move UpradeCallableReferences before FunctionReferenceLowering

--- compiler/testData/codegen/box/invokedynamic/serializable/multipleTopLevelFunRefs.kt
+++ compiler/testData/codegen/box/invokedynamic/serializable/multipleTopLevelFunRefs.kt
@@ -10,6 +10,12 @@
 // 8 java/lang/String\.equals
 
 // FILE: multipleTopLevelFunRefs.kt
+
+// No deduplication happens now, because each reference would generate it's own private
+// wrapper function. In principle, it's possible to deduplicate such functions,
+// at least in simple cases, where no adaptations happen. Than there would be only 4 private
+// funcions instead of 8, and only 4 cases in desirialization switch, leading to only 12 indy calls.
+
 import java.io.*
 
 fun plusK1(s: String) = s + "K"
